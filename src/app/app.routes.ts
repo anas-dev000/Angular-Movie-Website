f8112e225c6ceb938729cc4f9645b75b1c3d6fe5
Anas Ali Abd Elmoneim <anas.dev000@gmail.com>
2025-09-13 23:06:09 +0200

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+
+export const routes: Routes = [
+
+];
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiKey: '3fb8345ed39c96353f20a4407cc4d32d',
+  apiUrl: 'https://api.themoviedb.org/3',
+  imgBaseUrl: 'https://image.tmdb.org/t/p/w500',
+  apiReadAccessToken:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZmI4MzQ1ZWQzOWM5NjM1M2YyMGE0NDA3Y2M0ZDMyZCIsIm5iZiI6MTc1NzQwMjczNi4xMzc5OTk4LCJzdWIiOiI2OGJmZDY3MDVjMWU0OTY3ZWEwNmFhYTIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.MAk650UHx4dbYtCjy8HYBumtKB18Ugg34Wom2N1e7Xc',
+};
